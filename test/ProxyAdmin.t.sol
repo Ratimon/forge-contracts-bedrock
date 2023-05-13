e7cb0a26d81b93650815e271066baf745375fda3
Ratimon <ratixoxo@gmail.com>
2023-05-13 07:11:57 +0700

    Test(./test): Adding test cases for get & change ProxyAdmin and upgrade

--- test/ProxyAdmin.t.sol
+++ test/ProxyAdmin.t.sol
@@ -22,7 +22,7 @@ contract ProxyAdmin_Test is Test {
     uint256 ownerPrivateKey = vm.deriveKey(mnemonic, "m/44'/60'/0'/0/", 1); //  address = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8
     address owner = vm.envOr("DEPLOYER", vm.addr(ownerPrivateKey));
 
-    Deployer deployer;
+    Deployer deployerProcedue;
 
     Proxy proxy;
     L1ChugSplashProxy chugsplash;
@@ -32,14 +32,14 @@ contract ProxyAdmin_Test is Test {
     SimpleStorage implementation;
 
     function setUp() external {       
-        deployer = getDeployer();
-        deployer.setAutoBroadcast(false);
+        deployerProcedue = getDeployer();
+        deployerProcedue.setAutoBroadcast(false);
 
         DeployProxyAdminScript proxyAdminDeployments = new DeployProxyAdminScript();
         DeployAddressManagerScript addressManagerDeployments = new DeployAddressManagerScript();
 
         // vm.startPrank(owner);
-        deployer.activatePrank(vm.envAddress("DEPLOYER"));
+        deployerProcedue.activatePrank(vm.envAddress("DEPLOYER"));
 
         // Deploy the proxy admin
         admin = proxyAdminDeployments.deploy();
@@ -51,6 +51,7 @@ contract ProxyAdmin_Test is Test {
 
         // // Deploy the legacy AddressManager
         addressManager = addressManagerDeployments.deploy();
+        deployerProcedue.deactivatePrank();
     }
 
     modifier beforeEach() {
@@ -145,12 +146,10 @@ contract ProxyAdmin_Test is Test {
     }
 
     function getProxyImplementation(address payable _proxy) internal {
-        // vm.prank(address(0));
         {
             address impl = admin.getProxyImplementation(_proxy);
             assertEq(impl, address(0));
         }
-        // deployer.activatePrank(vm.envAddress("DEPLOYER"));
         vm.prank(owner, owner);
         admin.upgrade(_proxy, address(implementation));
 
@@ -160,134 +159,134 @@ contract ProxyAdmin_Test is Test {
         }
     }
 
-    // function test_erc1967GetProxyAdmin_succeeds() external {
-    //     getProxyAdmin(payable(proxy));
-    // }
+    function test_erc1967GetProxyAdmin_succeeds() external beforeEach {
+        getProxyAdmin(payable(proxy));
+    }
 
-    // function test_chugsplashGetProxyAdmin_succeeds() external {
-    //     getProxyAdmin(payable(chugsplash));
-    // }
+    function test_chugsplashGetProxyAdmin_succeeds() external beforeEach {
+        getProxyAdmin(payable(chugsplash));
+    }
 
-    // function test_delegateResolvedGetProxyAdmin_succeeds() external {
-    //     getProxyAdmin(payable(resolved));
-    // }
+    function test_delegateResolvedGetProxyAdmin_succeeds() external beforeEach {
+        getProxyAdmin(payable(resolved));
+    }
 
-    // function getProxyAdmin(address payable _proxy) internal {
-    //     address owner = admin.getProxyAdmin(_proxy);
-    //     assertEq(owner, address(admin));
-    // }
+    function getProxyAdmin(address payable _proxy) internal {
+        address proxyAdminOwner = admin.getProxyAdmin(_proxy);
+        assertEq(proxyAdminOwner, address(admin));
+    }
 
-    // function test_erc1967ChangeProxyAdmin_succeeds() external {
-    //     changeProxyAdmin(payable(proxy));
-    // }
+    function test_erc1967ChangeProxyAdmin_succeeds() external beforeEach {
+        changeProxyAdmin(payable(proxy));
+    }
 
-    // function test_chugsplashChangeProxyAdmin_succeeds() external {
-    //     changeProxyAdmin(payable(chugsplash));
-    // }
+    function test_chugsplashChangeProxyAdmin_succeeds() external beforeEach {
+        changeProxyAdmin(payable(chugsplash));
+    }
 
-    // function test_delegateResolvedChangeProxyAdmin_succeeds() external {
-    //     changeProxyAdmin(payable(resolved));
-    // }
+    function test_delegateResolvedChangeProxyAdmin_succeeds() external beforeEach {
+        changeProxyAdmin(payable(resolved));
+    }
 
-    // function changeProxyAdmin(address payable _proxy) internal {
-    //     ProxyAdmin.ProxyType proxyType = admin.proxyType(address(_proxy));
+    function changeProxyAdmin(address payable _proxy) internal {
+        ProxyAdmin.ProxyType proxyType = admin.proxyType(address(_proxy));
 
-    //     vm.prank(alice);
-    //     admin.changeProxyAdmin(_proxy, address(128));
+        vm.prank(owner);
+        admin.changeProxyAdmin(_proxy, address(128));
 
-    //     // The proxy is no longer the admin and can
-    //     // no longer call the proxy interface except for
-    //     // the ResolvedDelegate type on which anybody can
-    //     // call the admin interface.
-    //     if (proxyType == ProxyAdmin.ProxyType.ERC1967) {
-    //         vm.expectRevert("Proxy: implementation not initialized");
-    //         admin.getProxyAdmin(_proxy);
-    //     } else if (proxyType == ProxyAdmin.ProxyType.CHUGSPLASH) {
-    //         vm.expectRevert("L1ChugSplashProxy: implementation is not set yet");
-    //         admin.getProxyAdmin(_proxy);
-    //     } else if (proxyType == ProxyAdmin.ProxyType.RESOLVED) {
-    //         // Just an empty block to show that all cases are covered
-    //     } else {
-    //         vm.expectRevert("ProxyAdmin: unknown proxy type");
-    //     }
+        // The proxy is no longer the admin and can
+        // no longer call the proxy interface except for
+        // the ResolvedDelegate type on which anybody can
+        // call the admin interface.
+        if (proxyType == ProxyAdmin.ProxyType.ERC1967) {
+            vm.expectRevert("Proxy: implementation not initialized");
+            admin.getProxyAdmin(_proxy);
+        } else if (proxyType == ProxyAdmin.ProxyType.CHUGSPLASH) {
+            vm.expectRevert("L1ChugSplashProxy: implementation is not set yet");
+            admin.getProxyAdmin(_proxy);
+        } else if (proxyType == ProxyAdmin.ProxyType.RESOLVED) {
+            // Just an empty block to show that all cases are covered
+        } else {
+            vm.expectRevert("ProxyAdmin: unknown proxy type");
+        }
 
-    //     // Call the proxy contract directly to get the admin.
-    //     // Different proxy types have different interfaces.
-    //     vm.prank(address(128));
-    //     if (proxyType == ProxyAdmin.ProxyType.ERC1967) {
-    //         assertEq(Proxy(payable(_proxy)).admin(), address(128));
-    //     } else if (proxyType == ProxyAdmin.ProxyType.CHUGSPLASH) {
-    //         assertEq(L1ChugSplashProxy(payable(_proxy)).getOwner(), address(128));
-    //     } else if (proxyType == ProxyAdmin.ProxyType.RESOLVED) {
-    //         assertEq(addressManager.owner(), address(128));
-    //     } else {
-    //         assert(false);
-    //     }
-    // }
+        // Call the proxy contract directly to get the admin.
+        // Different proxy types have different interfaces.
+        vm.prank(address(128));
+        if (proxyType == ProxyAdmin.ProxyType.ERC1967) {
+            assertEq(Proxy(payable(_proxy)).admin(), address(128));
+        } else if (proxyType == ProxyAdmin.ProxyType.CHUGSPLASH) {
+            assertEq(L1ChugSplashProxy(payable(_proxy)).getOwner(), address(128));
+        } else if (proxyType == ProxyAdmin.ProxyType.RESOLVED) {
+            assertEq(addressManager.owner(), address(128));
+        } else {
+            assert(false);
+        }
+    }
 
-    // function test_erc1967Upgrade_succeeds() external {
-    //     upgrade(payable(proxy));
-    // }
+    function test_erc1967Upgrade_succeeds() external beforeEach {
+        upgrade(payable(proxy));
+    }
 
-    // function test_chugsplashUpgrade_succeeds() external {
-    //     upgrade(payable(chugsplash));
-    // }
+    function test_chugsplashUpgrade_succeeds() external beforeEach {
+        upgrade(payable(chugsplash));
+    }
 
-    // function test_delegateResolvedUpgrade_succeeds() external {
-    //     upgrade(payable(resolved));
-    // }
+    function test_delegateResolvedUpgrade_succeeds() external beforeEach {
+        upgrade(payable(resolved));
+    }
 
-    // function upgrade(address payable _proxy) internal {
-    //     vm.prank(alice);
-    //     admin.upgrade(_proxy, address(implementation));
+    function upgrade(address payable _proxy) internal {
+        vm.prank(owner);
+        admin.upgrade(_proxy, address(implementation));
 
-    //     address impl = admin.getProxyImplementation(_proxy);
-    //     assertEq(impl, address(implementation));
-    // }
+        address impl = admin.getProxyImplementation(_proxy);
+        assertEq(impl, address(implementation));
+    }
 
-    // function test_erc1967UpgradeAndCall_succeeds() external {
-    //     upgradeAndCall(payable(proxy));
-    // }
+    function test_erc1967UpgradeAndCall_succeeds() external beforeEach {
+        upgradeAndCall(payable(proxy));
+    }
 
-    // function test_chugsplashUpgradeAndCall_succeeds() external {
-    //     upgradeAndCall(payable(chugsplash));
-    // }
+    function test_chugsplashUpgradeAndCall_succeeds() external beforeEach {
+        upgradeAndCall(payable(chugsplash));
+    }
 
-    // function test_delegateResolvedUpgradeAndCall_succeeds() external {
-    //     upgradeAndCall(payable(resolved));
-    // }
+    function test_delegateResolvedUpgradeAndCall_succeeds() external beforeEach {
+        upgradeAndCall(payable(resolved));
+    }
 
-    // function upgradeAndCall(address payable _proxy) internal {
-    //     vm.prank(alice);
-    //     admin.upgradeAndCall(
-    //         _proxy,
-    //         address(implementation),
-    //         abi.encodeWithSelector(SimpleStorage.set.selector, 1, 1)
-    //     );
+    function upgradeAndCall(address payable _proxy) internal {
+        vm.prank(owner);
+        admin.upgradeAndCall(
+            _proxy,
+            address(implementation),
+            abi.encodeWithSelector(SimpleStorage.set.selector, 1, 1)
+        );
 
-    //     address impl = admin.getProxyImplementation(_proxy);
-    //     assertEq(impl, address(implementation));
+        address impl = admin.getProxyImplementation(_proxy);
+        assertEq(impl, address(implementation));
 
-    //     uint256 got = SimpleStorage(address(_proxy)).get(1);
-    //     assertEq(got, 1);
-    // }
+        uint256 got = SimpleStorage(address(_proxy)).get(1);
+        assertEq(got, 1);
+    }
 
-    // function test_onlyOwner_notOwner_reverts() external {
-    //     vm.expectRevert("Ownable: caller is not the owner");
-    //     admin.changeProxyAdmin(payable(proxy), address(0));
+    function test_onlyOwner_notOwner_reverts() external beforeEach {
+        vm.expectRevert("Ownable: caller is not the owner");
+        admin.changeProxyAdmin(payable(proxy), address(0));
 
-    //     vm.expectRevert("Ownable: caller is not the owner");
-    //     admin.upgrade(payable(proxy), address(implementation));
+        vm.expectRevert("Ownable: caller is not the owner");
+        admin.upgrade(payable(proxy), address(implementation));
 
-    //     vm.expectRevert("Ownable: caller is not the owner");
-    //     admin.upgradeAndCall(payable(proxy), address(implementation), hex"");
-    // }
+        vm.expectRevert("Ownable: caller is not the owner");
+        admin.upgradeAndCall(payable(proxy), address(implementation), hex"");
+    }
 
-    // function test_isUpgrading_succeeds() external {
-    //     assertEq(false, admin.isUpgrading());
+    function test_isUpgrading_succeeds() external beforeEach {
+        assertEq(false, admin.isUpgrading());
 
-    //     vm.prank(alice);
-    //     admin.setUpgrading(true);
-    //     assertEq(true, admin.isUpgrading());
-    // }
+        vm.prank(owner);
+        admin.setUpgrading(true);
+        assertEq(true, admin.isUpgrading());
+    }
 }
