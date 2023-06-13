b96b6ee4d58192a2e3d3eca30c8a4a81a52768fe
Ratimon <ratixoxo@gmail.com>
2023-06-13 14:15:51 +0700

    Feat (./script): Adding deploy script for OptimismPortalProxy

--- script/005_OptimismPortalProxy.sol
+++ script/005_OptimismPortalProxy.sol
@@ -1,27 +1,24 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity =0.8.15;
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.15;
 
-// import {DeployScript, Deployer} from "forge-deploy/DeployScript.sol";
-// import {DeployerFunctions} from "@generated/deployer/DeployerFunctions.g.sol";
+import {DeployScript, Deployer} from "forge-deploy/DeployScript.sol";
+import {DeployerFunctions} from "@generated/deployer/DeployerFunctions.g.sol";
 
-// import {ProxyAdmin} from "@main/universal/ProxyAdmin.sol";
-// // import {Proxy} from "@main/universal/Proxy.sol";
+import {ProxyAdmin} from "@main/universal/ProxyAdmin.sol";
+import {Proxy} from "@main/universal/Proxy.sol";
 
-// import {OptimismPortal} from "@main/L1/OptimismPortal.sol";
+contract DeployOptimismPortalProxyScript is DeployScript {
+    using DeployerFunctions for Deployer;
 
+    ProxyAdmin proxyAdmin;
 
-// contract DeployOptimismPortalProxyScript is DeployScript {
-//     using DeployerFunctions for Deployer;
+    function deploy() external returns (Proxy) {
+        proxyAdmin = ProxyAdmin(deployer.getAddress("ProxyAdmin"));
 
-//     ProxyAdmin proxyAdmin;
-
-//     function deploy() external returns (OptimismPortal) {
-//         proxyAdmin = AddressManager(deployer.getAddress("ProxyAdmin"));
-
-//         return ResolvedDelegateProxy(
-//             deployer.deploy_O
-//                 "Proxy__OVM_L1CrossDomainMessenger", addressManager, "OVM_L1CrossDomainMessenger"
-//             )
-//         );
-//     }
-// }
+        return Proxy(
+            deployer.deploy_Proxy(
+                "OptimismPortalProxy", address(proxyAdmin)
+            )
+        );
+    }
+}
